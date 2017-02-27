fc85e35d71cfcbf4ec49200cb33f99ca714c9042
Joanna <joanna.piotrowska.wrzos@gmail.com>
2017-02-27 22:05:06 +0100

    counting balloons; displaying counter and balloons

--- js/main.js
+++ js/main.js
@@ -7,13 +7,43 @@ $("#slider").ionRangeSlider({
     postfix: "cm"
 });
 
-$("button[type=submit]").click(function() {
-    var $donuts = $("input#donuts").val();
-    var $height = $("input[name='height']:checked").val();
-    var $colours = $("input[name='colours']:checked").val();
-    var $sliderValue = $("#slider").prop("value");
+function howManyBalloons($sliderValue,$donuts,$elevation) {
+    return 10+Math.ceil($sliderValue*$donuts*$elevation*0.1);
+}
 
-    console.log($sliderValue + " " +$donuts +  " " + $height + " " + $colours);
+function addBalloon($counter,$colours) {
+    $(".balloonGroup").empty();
+    for(var i=0; i<$counter; i++) {
+        var $newBalloon = $(".balloonPrototype svg").clone();
+        $(".balloonGroup").append($newBalloon);
+        
+    }
+    $bgColor = $("."+$colours).css("background-color");
+    $("svg>g>g").css("fill",$bgColor);
+}
+
+$("button[type=submit]").click(function() {
+    $(".test").fadeOut(600);
+    $(".result").delay(600).fadeIn(600);
+    var $sliderValue = $("#slider").prop("value");
+    var $donuts = $("input#donuts").val();
+    var $elevation = $("input[name='height']:checked").val();
+    var $colours = $("input[name='colours']:checked").val();
+
+    var $counter = howManyBalloons($sliderValue,$donuts,$elevation);
+    $("#balloon-color").text($colours).addClass($colours);
+    $("#counter").prop("counter",0).animate({
+        "counter": $counter
+    },{
+        duration: 3000,
+        easing: 'swing',
+        step: function (now) {
+            $(this).text(Math.ceil(now));
+        }
+    });
+
+    addBalloon($counter,$colours);
+    
     return false;
 });
 
